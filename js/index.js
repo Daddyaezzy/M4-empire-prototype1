9d3e0ac85369c8d8a959c87126967d7249d6b5ad
Fajobi Anselm <pulumbu11@gmail.com>
2022-07-02 20:52:10 +0100

    update

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var nav = document.getElementById("nav");
+
+        function showMenu(){
+            nav.style.display = "block"
+        }
+        function hideMenu(){
+            nav.style.display = "none"
+        }
+
+
+const nav = document.getElementById("nav")
+
+window.onscroll = function(){ 
+    if(document.body.scrollTop>=150)
+    {nav.classList.add("colorNav")
+}
+
+    else{nav.classList.remove("colorNav")}
+}

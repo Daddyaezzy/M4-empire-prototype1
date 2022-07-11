edf1e37aa2e85fece4600554e5ffec660fc06949
Fajobi Anselm <pulumbu11@gmail.com>
2022-07-11 16:14:07 +0100

    update

---
+++ form-handler.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'pulumbu11@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User name: $name.\n".
+                "User email: $visitor_email.\n".
+                  "subject: $subject.\n".
+                  "User message: $message.\n";
+
+$to = 'm4empire.general@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contact-us.html");
+
+?>
